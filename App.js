b6ad6ad6d3bb8a6d8c5657bac2c50739b00e546a
thaiduongPacificOcean <thaibinhduong56390@gmail.com>
2024-03-06 21:59:02 +0700

    Food-app

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { NavigationContainer} from '@react-navigation/native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './src/components/Home';
+import Details from './src/components/Details';
+
+
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions= {{headerShown : false}}>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Details' component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
